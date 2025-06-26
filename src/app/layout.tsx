7ce554fbf05d6f9d55961269a5cefc6c6b033c13
Ju777 <julien.tamil@gmail.com>
2025-06-26 17:54:45 +0200

    Optimize accessibility.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
